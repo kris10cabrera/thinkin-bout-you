f32173c8d51c2d183db4b55dbcf6117e963f365a
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-04-06 19:36:56 +0200

    Create Overlay.tsx

---
+++ components/Overlay.tsx
@@ -0,0 +1,28 @@
+export default function Overlay() {
+  return (
+    <>
+      <style>
+        {`
+          @keyframes fadeIn {
+            0% { opacity: 1; }
+            100% { opacity: 0; }
+          }
+        `}
+      </style>
+      <div
+        className={` 
+          fixed 
+          inset-0 
+          z-50 
+          pointer-events-none 
+          bg-neutral-900/20 
+          backdrop-blur-md
+          opacity-0
+          transition-opacity
+          duration-[5000s]
+          animate-[fadeIn_5s]
+        `}
+      />
+    </>
+  )
+}
