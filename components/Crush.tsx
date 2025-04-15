c403c6dabf27e54863cf1d0af3a5b1990e98e469
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-04-15 23:07:24 +0200

    Update Crush.tsx

--- components/Crush.tsx
+++ components/Crush.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils"
-import { Heart } from "./chamber"
 
 interface CrushProps {
   index: number
@@ -12,19 +11,18 @@ export default function Crush(props: CrushProps) {
       key={index}
       className={cn(
         (index % 3 === 0 || index % 4 === 0) && "mr-40",
-        "relative z-30 bg-white/80 p-2 uppercase "
+        "relative z-30 bg-[#f6ffdb] md:bg-white/80 p-2 uppercase "
       )}
     >
       <span className="text-9xl font-tram relative">
         {crush[0]}
         <span className="text-7xl">&</span>
         {crush[1]}
-        <span className="text-base font-kosugi-maru a">
+        <span className="text-base font-kosugi-maru whitespace-nowrap">
           {crush[0]}
           <span>&</span>
           {crush[1]} forever
         </span>
-        <Heart />
       </span>
     </div>
   )
