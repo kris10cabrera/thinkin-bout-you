f1b0feb2a731c63bb385222cf573b76f9937622b
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-04-15 23:14:51 +0200

    updates

--- components/Crush.tsx
+++ components/Crush.tsx
@@ -11,17 +11,15 @@ export default function Crush(props: CrushProps) {
       key={index}
       className={cn(
         (index % 3 === 0 || index % 4 === 0) && "mr-40",
-        "relative z-30 bg-[#f6ffdb] md:bg-white/80 p-2 uppercase "
+        "relative z-30  bg-white/80 p-2 uppercase shadow-[0_0_20px_9px_white] rounded-md"
       )}
     >
-      <span className="text-9xl font-tram relative">
+      <span className="text-7xl md:text-9xl font-tram relative">
         {crush[0]}
-        <span className="text-7xl">&</span>
+        <span className="text-4xl md:text-7xl">&</span>
         {crush[1]}
         <span className="text-base font-kosugi-maru whitespace-nowrap">
-          {crush[0]}
-          <span>&</span>
-          {crush[1]} forever
+          forever
         </span>
       </span>
     </div>
