4102634864f85d52be9173a917317504103782f8
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-05-04 17:32:05 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,25 +17,26 @@ export default function Home() {
       <main className="">
         <Overlay />
         <div className="text">
-          <div className="bg-[#ffffffa1] border-white border-4 mb-8">
-            <h1 className="bg-gradient z-40 relative font-tram inline-block text-[20vw] leading-[.8]">
-              are you in love?
-            </h1>
-            <p className="pl-2 text-base sm:text-6xl bg-gradient  z-40 relative leading-none text-[6vw] py-6 max-w-[40ch]  pointer-events-none">
-              carve your names into this website to celebrate your love,
-              forever.
-            </p>
-            <p className="z-40 pl-2  relative leading-none text-base lg:text-4xl bg-gradient">
-              (pretend you are carving your names in a tree)
-            </p>
-            <p className="bg-gradient pl-2  border  border-black inline-block rounded  p-2 lg:p-3 z-40 relative my-8 leading-none lg:text-2xl">
-              accepting 333 crushes total
-            </p>{" "}
-            <p className="bg-gradient z-40 pl-2  relative leading-none  text-base lg:text-3xl max-w-[40ch] pb-8">
-              reminder that love is all around us! especially on this website
-              (2025). xoxo!
-            </p>
-          </div>
+          <h1 className="tittttle z-40 relative font-tram inline-block text-[20vw] leading-[.8]" style={{
+
+            padding: "20px"
+          }}>
+            are you in love?
+          </h1>
+          <p className="pl-2 text-base sm:text-6xl bg-gradient  z-40 relative leading-none text-[6vw] py-6 max-w-[40ch]  pointer-events-none">
+            carve your names into this website to celebrate your love,
+            forever.
+          </p>
+          <p className="z-40 pl-2  relative leading-none text-base lg:text-4xl bg-gradient">
+            (pretend you are carving your names in a tree)
+          </p>
+          <p className="bg-gradient pl-2  border  border-black inline-block rounded  p-2 lg:p-3 z-40 relative my-8 leading-none lg:text-2xl">
+            accepting 333 crushes total
+          </p>{" "}
+          <p className="bg-gradient z-40 pl-2  relative leading-none  text-base lg:text-3xl max-w-[40ch] pb-8">
+            reminder that love is all around us! especially on this website
+            (2025). xoxo!
+          </p>
         </div>
         <Form />
         <p className="text-sm border border-dashed border-black bg-white inline-block px-1 mt-4 mb-4 font-kosugiMari z-50 relative ">
